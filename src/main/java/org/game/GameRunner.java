6787ef91d3ed4f1e788fbb39dacfbe8aa04d4d9a
15ambm <alexandermason98@gmail.com>
2024-12-10 16:33:38 -0500

    - improved CLI formatting

--- src/main/java/org/game/GameRunner.java
+++ src/main/java/org/game/GameRunner.java
@@ -8,6 +8,7 @@ class GameRunner {
     private final Board playerOneBoard;
     private final Board playerTwoBoard;
     private boolean gameOver = false;
+    private int currentPlayer = 1;
 
     public GameRunner() {
         this.playerOneBoard = new Board();
@@ -15,9 +16,13 @@ class GameRunner {
     }
 
     public void run() {
+        System.out.println("\nWelcome to Battleship CLI!" +
+                "\nPlayer one will begin by entering the coordinates of their ships, followed by player 2." +
+                "\nYou can see the game board below:");
         placeShips(playerOneBoard);
+        waitForAndSetNextPlayer();
         placeShips(playerTwoBoard);
-        waitForNextPlayer();
+        waitForAndSetNextPlayer();
         beginGame();
     }
 
@@ -27,7 +32,7 @@ class GameRunner {
         int shipIndex = 0;
         board.printFullBoard();
         do {
-            System.out.printf("\nEnter the coordinates of the %s (%d cells):\n> ", ships.get(shipIndex).getName(), ships.get(shipIndex).getLength());
+            System.out.printf("\nPlayer %d, enter the coordinates of the %s (%d cells):\n> ", currentPlayer, ships.get(shipIndex).getName(), ships.get(shipIndex).getLength());
             input = scanner.nextLine();
             String[] coordinates = input.toUpperCase().trim().split("\\s+");
             try {
@@ -38,47 +43,46 @@ class GameRunner {
                 System.out.println("Error: " + e.getMessage());
             }
         } while (!input.equals("exit") && shipIndex < ships.size());
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     }
 
     private void beginGame() {
         System.out.println("\nThe game starts!");
-        int currentPlayer = 1;
-
         while (!gameOver) {
-            printBoards(currentPlayer);
+            printBoards();
             System.out.printf("\nPlayer %d, it's your turn:", currentPlayer);
-            attack(currentPlayer);
-            waitForNextPlayer();
-            currentPlayer = currentPlayer == 1 ? 2 : 1;
+            attack();
+            waitForAndSetNextPlayer();
         }
     }
 
-    private void attack(int currentPlayer) {
+    private void attack() {
         Board board = currentPlayer == 1 ? playerTwoBoard : playerOneBoard;
         System.out.print("\n> ");
         String input = scanner.nextLine();
+        if (input.equals("exit")) System.exit(0);
         String coordinate = input.toUpperCase().trim();
         try {
             String actionResult = board.shootAtShip(coordinate);
             if (actionResult.equals("win")) {
                 board.printFullBoard();
-                System.out.println("\nYou sank the last ship. You won. Congratulations!");
-                this.gameOver = true;
-                return;
+                System.out.printf("\nCongrats player %s, you sank the last ship! You win!\n", currentPlayer);
+                System.exit(0);
             }
             System.out.println(actionResult);
         } catch (RuntimeException e) {
             System.out.println("Error: " + e.getMessage());
-            attack(currentPlayer);
+            attack();
         }
     }
 
-    private void waitForNextPlayer() {
-        System.out.println("Press Enter and pass the move to another player");
+    private void waitForAndSetNextPlayer() {
+        System.out.println("\nPress Enter and pass the move to another player");
+        currentPlayer = currentPlayer == 1 ? 2 : 1;
         scanner.nextLine();
     }
 
-    private void printBoards(int currentPlayer) {
+    private void printBoards() {
         if (currentPlayer == 1) {
             playerTwoBoard.printBoardWithFog();
             System.out.print("---------------------");
